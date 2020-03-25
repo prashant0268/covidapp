d67d6a8a28ec4f2f3b0c4c7b18ab054499a64990
Prashant Sapkal <psapkal@etsy.com>
2020-03-25 10:20:28 -0400

    update

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -25,7 +25,7 @@ function Home() {
     <div>
       <div>
         <Link to="/addpolicy">Add Policy</Link>
-        <span>Hello World!!</span>
+        <span>Hello World!!!</span>
       </div>
     </div>
   );
